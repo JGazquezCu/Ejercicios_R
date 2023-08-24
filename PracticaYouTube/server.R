8f3c7bca07a4d5a5fe273c401ce719336c408e16
Jose Antonio Gazquez Cuenca <joseantoniogazquezcuenca@MacBook-Pro-de-Jose.local>
2023-08-24 23:25:31 +0200

    Empezamos a practicar

---
+++ PracticaYouTube/server.R
@@ -0,0 +1,45 @@
+
+library(shiny)
+
+
+shinyServer(function(input, output) {
+  
+  output$single_slider_io <- renderPrint({
+    print(input$slider_input)
+    print(str(input$slider_input))
+  })
+  
+  output$range_slider_io <- renderPrint({
+    input$range_slider
+  })
+  
+  output$numeric_io <- renderPrint({
+    input$numeric_input
+  })
+  
+  output$date_io <- renderPrint({
+    input$single_date_input
+  })
+  
+  output$range_date_io <- renderPrint({
+    input$date_range_input
+  })
+  
+  output$chk_box_io <- renderPrint({
+    input$single_chk_box
+  })
+  
+  output$chk_box_group_io <- renderPrint({
+    input$chck_Box_group
+  })
+  
+  output$radio_io <- renderPrint({
+    input$radio_input
+  })
+  
+  
+  output$ab_io <- renderPrint({
+    input$btn_ok
+  })
+  
+})
---
+++ PracticaYouTube/ui.R
@@ -0,0 +1,78 @@
+### https://shiny.rstudio.com/images/shiny-cheatsheet.pdf
+
+
+library(shiny)
+library(lubridate)
+
+
+shinyUI(fluidPage(
+  
+  
+  titlePanel("Catalogo de Inputs de Shiny"),
+  
+  
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput("slider_input","Seleccione un valor:",
+                  min = 0,
+                  max = 100,
+                  value = 25,
+                  step = 1,
+                  animate= TRUE,
+                  post = '%'),
+      sliderInput("range_slider","Sleccione rango:",
+                  min = 0,
+                  max=100,
+                  value = c(0,100)),
+      numericInput("numeric_input","Seleccio edad:",
+                   value = 10,min=0,max=150),
+      dateInput("single_date_input","Ingrese la fecha:", 
+                value = today(),
+                min = today()-365,
+                max=today(),
+                language = "es",
+                weekstart = 1,),
+      dateRangeInput("date_range_input", 
+                     "Seleccione el rango de fechas:",
+                     start = today()-30,
+                     end = today(),
+                     format = 'dd/mm/yyyy', 
+                     language = 'es',separator = "a"),
+      checkboxInput("single_chk_box","Desea recibir informacion:",
+                    value = FALSE ),
+      checkboxGroupInput("chck_Box_group","Seleccione tipo:",
+                         choices = c("Carro","Camioneta","Moto","tractor"),
+                         selected = NULL  ),
+      radioButtons("radio_input", "Seleccione uno",
+                   choices = c("primaria","secundaria","universidad"),
+                   selected = NULL ),
+      actionButton("btn_ok","Refrescar"),
+      actionButton("btn_salir","Salir"),
+      actionLink("link_seguir","siguiente"),
+      submitButton("Ejecutar")
+    ),
+    
+    
+    mainPanel(
+      h1("Salidas de los inputs de shiny"),
+      h2("slider IO"),
+      verbatimTextOutput("single_slider_io"),
+      h2("slider con rango"),
+      verbatimTextOutput("range_slider_io"),
+      h2("Salida Numeric Input"),
+      verbatimTextOutput("numeric_io"),
+      h2("Salida Date Input"),
+      verbatimTextOutput("date_io"),
+      h2("Salida Range Date Input"),
+      verbatimTextOutput("range_date_io"),
+      h2("Salida Check Box"),
+      verbatimTextOutput("chk_box_io"),
+      h2("salido del check box group"),
+      verbatimTextOutput("chk_box_group_io"),
+      h2("Salida del radio button"),
+      verbatimTextOutput("radio_io"),
+      h2("Salido Actin Button"),
+      verbatimTextOutput("ab_io")
+    )
+  )
+))
